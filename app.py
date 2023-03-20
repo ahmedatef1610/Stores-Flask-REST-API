c98f7f36d027ba7868ce1416ceef2741b36348a1
ahmed atef <ahmedatef1610@gmail.com>
2023-03-20 23:56:34 +0200

    first commit

--- app.py
+++ app.py
@@ -117,10 +117,6 @@ def create_app(db_url=None):
 
 ####################################################################
 
-# if __name__ == '__main__':
-#     app = create_app()
-#     # @app.route('/')
-#     # def index():
-#     #     return render_template('index.html')
-#     # app.run(host='127.0.0.1', port=8080, debug=True)
-#     app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,5 @@ flask db upgrade
 
 # exec gunicorn --bind 0.0.0.0:8080 "app:app"
 
-exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
+exec gunicorn --bind 0.0.0.0:8080 "app:create_app()"
 
