83d91cee4f9798c63a21f6f91fd291effb91ebe7
ahmed atef <ahmedatef1610@gmail.com>
2023-03-20 23:46:07 +0200

    first commit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,8 @@
 flask db upgrade
 
 # exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
-exec gunicorn --bind 0.0.0.0:8080 "app:app"
+
+# exec gunicorn --bind 0.0.0.0:8080 "app:app"
+
+exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
+
